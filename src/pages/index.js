ca3d75abaf0eed1a628d04457009c7ad953c3d2b
ZzlinhzZ <llinh1413@gmail.com>
2023-11-01 13:32:27 +0700

    abc

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,36 +6,109 @@ export default function Home() {
   // Hiển thị mã ngôn ngữ hiện tại
   const { t } = useTranslation("translationHome");
   const { i18n } = useTranslation();
-  console.log(i18n.language);
+  const LANG_CODE = i18n.language;
+  // console.log(i18n.language);
   const programs = [
-    // Chương trình 1
     {
-      title: "Nấu ăn cho em 1",
-      time: "9/2023 - 3/2024",
-      description:
-        "Trẻ em là nhóm dễ bị tổn thương và chịu ảnh hưởng mạnh từ nghèo đói...",
-      progress: {
-        current: "18.000.000đ",
-        goal: "20.000.000đ",
-        percentage: "80%",
+      vi: {
+        title: "Nấu ăn cho em 1",
+        time: "9/2023 - 3/2024",
+        description: "Trẻ em là nhóm dễ bị tổn thương và chịu ảnh hưởng mạnh từ nghèo đói...",
+        progress: {
+          current: "18.000.000đ",
+          goal: "20.000.000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
       },
-      contributors: 34562,
-      videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      en: {
+        title: "Cooking for Kids 1",
+        time: "9/2023 - 3/2024",
+        description: "Children are a vulnerable group and are heavily affected by poverty...",
+        progress: {
+          current: "18,000,000đ",
+          goal: "20,000,000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      },
+      ja: {
+        title: "子供のための料理 1",
+        time: "9/2023 - 3/2024",
+        description: "子供は脆弱なグループであり、貧困の影響を強く受けています...",
+        progress: {
+          current: "18,000,000đ",
+          goal: "20,000,000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      },
+      fr: {
+        title: "Cuisiner pour les enfants 1",
+        time: "9/2023 - 3/2024",
+        description: "Les enfants sont un groupe vulnérable et sont fortement touchés par la pauvreté...",
+        progress: {
+          current: "18,000,000đ",
+          goal: "20,000,000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      },
     },
-    // Chương trình 2
     {
-      title: "Nấu ăn cho em 2",
-      time: "9/2023 - 3/2024",
-      description:
-        "Trẻ em là nhóm dễ bị tổn thương và chịu ảnh hưởng mạnh từ nghèo đói...",
-      progress: {
-        current: "20.000.000đ",
-        goal: "20.000.000đ",
-        percentage: "100%",
+      vi: {
+        title: "Nấu ăn cho em 1",
+        time: "9/2023 - 3/2024",
+        description: "Trẻ em là nhóm dễ bị tổn thương và chịu ảnh hưởng mạnh từ nghèo đói...",
+        progress: {
+          current: "18.000.000đ",
+          goal: "20.000.000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
       },
-      contributors: 34562,
-      videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
-    },
+      en: {
+        title: "Cooking for Kids 1",
+        time: "9/2023 - 3/2024",
+        description: "Children are a vulnerable group and are heavily affected by poverty...",
+        progress: {
+          current: "18,000,000đ",
+          goal: "20,000,000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      },
+      ja: {
+        title: "子供のための料理 1",
+        time: "9/2023 - 3/2024",
+        description: "子供は脆弱なグループであり、貧困の影響を強く受けています...",
+        progress: {
+          current: "18,000,000đ",
+          goal: "20,000,000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      },
+      fr: {
+        title: "Cuisiner pour les enfants 1",
+        time: "9/2023 - 3/2024",
+        description: "Les enfants sont un groupe vulnérable et sont fortement touchés par la pauvreté...",
+        progress: {
+          current: "18,000,000đ",
+          goal: "20,000,000đ",
+          percentage: "80%",
+        },
+        contributors: 34562,
+        videoUrl: "https://www.youtube.com/embed/543jwFfCZvg?si=YJz93RMtk3DgjiTk",
+      },
+    }
   ];
   
   return (
@@ -44,7 +117,7 @@ export default function Home() {
       <SectionBigProject />
       <div className="text-center py-16">
         <h1 className="text-3xl font-bold">{t("detail.title")}</h1>
-        <h2 className="text-3xl font-bold">{t("index.next")}</h2>
+        {/* <h2 className="text-3xl font-bold">{t("index.next")}</h2> */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-40 py-8">
         {programs.map((program, index) => (
@@ -53,22 +126,22 @@ export default function Home() {
             id={`donate${index}`}
             key={index}
           >
-            <h3 className="text-2xl font-bold mb-4">{program.title}</h3>
+            <h3 className="text-2xl font-bold mb-4">{program[LANG_CODE].title}</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
                 <p>
-                  <strong>{t("index.time")}: {program.time}</strong>
+                  <strong>{t("index.time")}: {program[LANG_CODE].time}</strong>
                 </p>
                 <p className="text-justify text-gray-700 my-4">
-                  {program.description}
+                  {program[LANG_CODE].description}
                 </p>
               </div>
-              <div className="w-1/2">
+              <div className="w-1/2 ">
                 <iframe
                   className="rounded-lg shadow-md"
                   width="300"
                   height="200"
-                  src={programs[0].videoUrl}
+                  src={program[LANG_CODE].videoUrl}
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -78,10 +151,10 @@ export default function Home() {
             </div>
             <div className="flex items-center justify-between mb-4">
               <div className="text-sm font-semibold text-gray-600">
-                {program.progress.current} / {program.progress.goal}
+                {program[LANG_CODE].progress.current} / {program[LANG_CODE].progress.goal}
               </div>
               <div className="text-sm font-semibold text-green-500">
-                {program.progress.percentage}
+                {program[LANG_CODE].progress.percentage}
               </div>
             </div>
             <div className="relative pt-1">
@@ -93,13 +166,92 @@ export default function Home() {
                 </div>
                 <div className="text-right">
                   <span className="text-xs font-semibold inline-block text-green-600">
-                    {program.contributors}
+                    {program[LANG_CODE].contributors}
                   </span>
                 </div>
               </div>
               <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-green-200">
                 <div
-                  style={{ width: program.progress.percentage }}
+                  style={{ width: program[LANG_CODE].progress.percentage }}
+                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
+                ></div>
+              </div>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <button className="btn bg-blue-500 hover:bg-blue-700 text-white mr-2">
+              {t("SectionBigProject.detail")}
+              </button>
+              <button className="btn bg-green-500 hover:bg-green-700 text-white mr-2">
+                {t("index.donate")}
+              </button>
+              <a href="about.html" className="text-blue-500 hover:underline">
+                {t("index.report")}
+              </a>
+            </div>
+          </div>
+        ))}
+        {/* Hiển thị video */}
+      </div>
+
+      {/* dự án đang thực hiện */}
+      <div className="text-center py-16">
+        {/* <h1 className="text-3xl font-bold">{t("detail.title")}</h1> */}
+        <h2 className="text-3xl font-bold">{t("index.next")}</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-40 py-8">
+        {programs.map((program, index) => (
+          <div
+            className="bg-white p-8 rounded-lg shadow-md"
+            id={`donate${index}`}
+            key={index}
+          >
+            <h3 className="text-2xl font-bold mb-4">{program[LANG_CODE].title}</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <p>
+                  <strong>{t("index.time")}: {program[LANG_CODE].time}</strong>
+                </p>
+                <p className="text-justify text-gray-700 my-4">
+                  {program[LANG_CODE].description}
+                </p>
+              </div>
+              <div className="w-1/2 ">
+                <iframe
+                  className="rounded-lg shadow-md"
+                  width="300"
+                  height="200"
+                  src={program[LANG_CODE].videoUrl}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+            <div className="flex items-center justify-between mb-4">
+              <div className="text-sm font-semibold text-gray-600">
+                {program[LANG_CODE].progress.current} / {program[LANG_CODE].progress.goal}
+              </div>
+              <div className="text-sm font-semibold text-green-500">
+                {program[LANG_CODE].progress.percentage}
+              </div>
+            </div>
+            <div className="relative pt-1">
+              <div className="flex mb-2 items-center justify-between">
+                <div>
+                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-600 bg-green-200">
+                    {t("index.donation")}
+                  </span>
+                </div>
+                <div className="text-right">
+                  <span className="text-xs font-semibold inline-block text-green-600">
+                    {program[LANG_CODE].contributors}
+                  </span>
+                </div>
+              </div>
+              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-green-200">
+                <div
+                  style={{ width: program[LANG_CODE].progress.percentage }}
                   className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
                 ></div>
               </div>
@@ -120,5 +272,7 @@ export default function Home() {
         {/* Hiển thị video */}
       </div>
     </div>
+
+    // đang thực hiện
   );
 }
