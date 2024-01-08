22fa15ba7a9388eabf7ef453b8075bf196dea0d4
lvnh2003 <your-email@example.com>
2024-01-08 17:26:01 +0700

    fix UI and add new icon scroll top page

--- src/components/GoToTop.js
+++ src/components/GoToTop.js
@@ -35,10 +35,10 @@ function GoToTop() {
     <div>
       {isVisible && (
         <div
-          className="scroll-to-top fixed bottom-0 right-0 mb-20 mr-20 z-10 cursor-pointer text-white text-2xl bg-green-500 hover:bg-blue-700 py-2 px-4 rounded-full shadow-lg w-16 h-16 flex items-center justify-center"
+          className="scroll-to-top fixed bottom-0 right-0 mb-20 mr-20 z-10 cursor-pointer text-white text-2xl bg-green-500 hover:bg-pink-200 py-2 px-4 rounded-full shadow-lg w-16 h-16 flex items-center justify-center"
           onClick={scrollToTop}
         >
-          <div onClick={scrollToTop}>Top</div>
+          <div onClick={scrollToTop}>&#9757;</div>
         </div>
       )}
     </div>
