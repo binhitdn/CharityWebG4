d498f5e1a4dc146c2599ea816cc23e0ada5f8bfe
ZzlinhzZ <llinh1413@gmail.com>
2024-01-08 23:53:35 +0700

    fix footer

--- src/components/Layout/footer.js
+++ src/components/Layout/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaPhoneAlt, FaMapMarkedAlt, FaRegClock, FaMapMarkerAlt, FaPhone, FaPhoneSquareAlt, FaVoicemail, FaCalendarWeek, FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaPhoneAlt, FaMapMarkedAlt, FaRegClock, FaMapMarkerAlt, FaPhone, FaVoicemail, FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import Image from "next/image";
 import COLORS from "../../constant/color";
 import { useTranslation } from "react-i18next";
@@ -12,47 +12,15 @@ const Footer = () => {
       <div className="py-16 px-16" style={{ backgroundColor: COLORS.secondary }}>
         <div className="container mx-auto flex flex-col space-y-24">
           <div className="grid lg:grid-cols-3 gap-20">
-            <div className="col-span-1 flex flex-row space-x-3">
-              <FaMapMarkedAlt className="w-12 h-12 text-amber-600 p-1" />
-              <div className="space-y-4">
-                <p className="text-white">{t("footer.address-title")}</p>
-                <p className="text-white font-medium">{t("footer.address")}</p>
-                <div className="col-span-1 flex flex-row space-x-3">
-                  <FaPhoneAlt className="w-12 h-12 text-amber-600 p-1" />
-                  <div className="space-y-2">
-                    <p className="text-white">{t("footer.phone")}</p>
-                    <p className="text-white font-medium">+84 123 123 124</p>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div className="col-span-1 flex flex-row space-x-3">
-              <FaPhoneAlt className="w-12 h-12 text-amber-600 p-1" />
-              <div className="space-y-2">
-                <p className="text-white">{t("footer.phone")}</p>
-                <p className="text-white font-medium">+84 123 123 124</p>
-              </div>
-            </div>
-            <div className="col-span-1 flex flex-row space-x-3">
-              <FaRegClock className="w-12 h-12 text-amber-600 p-1" />
-              <div className="space-y-2">
-                <p className="text-white">{t("footer.working_title")}</p>
-                <p className="text-white font-medium">{t("footer.working")}</p>
-              </div>
-            </div>
-          </div>
-          <div className="grid lg:grid-cols-4 gap-8">
             <div className="col-span-1 space-y-8">
-              <Image src="/logo.png" width={100} height={100} />
-              <p className="text-white">{t("footer.mokuteki")}</p>
-            </div>
-            <div className="col-span-1 space-y-8"></div>
-            <div className="col-span-1 space-y-8">
-              <div className="flex flex-col space-y-2">
-                <p className="text-white font-medium">{t("footer.mokuteki")}</p>
-                <p className="border-solid border border-amber-500 w-1/4"></p>
+              <div className="col-span-1 flex flex-row items-center space-x-3">
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <Image src="/logo.png" width={100} height={100} />
+                </div>
+                <p className="text-white">SMILE EYE CHARITY</p>
               </div>
+
               <div className="flex flex-col space-y-4">
                 <div className="flex flex-row items-center space-x-4">
                   <FaMapMarkerAlt className="w-6 h-6 text-white font-light" />
@@ -60,41 +28,41 @@ const Footer = () => {
                 </div>
                 <div className="flex flex-row items-center space-x-4">
                   <FaPhone className="w-6 h-6 text-white" />
-                  <p className="text-white">(0481) 123 987 2411</p>
-                </div>
-                <div className="flex flex-row items-center space-x-4">
-                  <FaPhoneSquareAlt className="w-6 h-6 text-white" />
-                  <p className="text-white">(0481) 123 987 2412</p>
+                  <p className="text-white">0962 414 125</p>
                 </div>
                 <div className="flex flex-row items-center space-x-4">
                   <FaVoicemail className="w-6 h-6 text-white" />
-                  <p className="text-white">contact@autoride.com</p>
-                </div>
-                <div className="flex flex-row items-center space-x-4">
-                  <FaCalendarWeek className="w-6 h-6 text-white" />
-                  <p className="text-white">{t("footer.working")}</p>
+                  <p className="text-white">dinhdongphuong@gmail.com</p>
                 </div>
               </div>
             </div>
-            <div className="col-span-1 space-y-8">
-              <div className="flex flex-col space-y-2">
-                <p className="text-white font-medium">SUBSCRIBE</p>
-                <p className="border-solid border border-amber-500 w-1/4"></p>
+            <div className="col-span-1 flex flex-row space-x-3">
+              <div className="col-span-1 space-y-8">
+                <div className="col-span-1 flex flex-row items-center space-x-3">
+                  <p className="text-white" style={{ marginTop: "30px" }}>{t("footer.company")}</p>
+                </div>
+
+                <div className="flex flex-col space-y-4">
+                  <div className="flex flex-row items-center space-x-4">
+                    <p className="text-white" style={{ marginTop: "30px" }}>{t("footer.companyinfo")}</p>
+                  </div>
+                  <div className="flex flex-row items-center space-x-4">
+                    <p className="text-white">{t("footer.tax")}: 4001273487</p>
+                  </div>
+                  <div className="flex flex-row items-center space-x-4">
+                    <p className="text-white">{t("footer.bank")}: MB bank, 020166669999, Smile Eye Charity</p>
+                  </div>
+                  <div className="flex flex-row items-center space-x-4">
+                    <p className="text-white">Techcombank, 22296699, Smile Eye Charity</p>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col space-y-8">
-                <input
-                  type="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  placeholder="binh@gmail.com"
-                  required
-                />
-                <button
-                  type="submit"
-                  className="text-white bg-amber-700 hover:bg-amber-800 focus:ring-4 focus:ring-blue-300 font-medium w-full sm:w-auto px-5 py-2.5 text-center"
-                >
-                  Submit
-                </button>
+            </div>
+            <div className="col-span-1 flex flex-row space-x-3" style={{ marginTop: "30px" }}>
+              <div className="space-y-2">
+                <p className="text-white">{t("footer.working_title")}</p>
+                <p className="text-white font-medium" style={{ marginTop: "80px" }}>{t("footer.working")}</p>
+                <p className="text-white font-medium" style={{ marginTop: "30px" }}>{t("footer.support")}: 24/7</p>
               </div>
             </div>
           </div>
